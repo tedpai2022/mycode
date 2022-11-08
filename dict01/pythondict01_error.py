b04feb795fc510647972f868814d295f6df1fe16
tedpai2022 <gtedpai1415@gmail.com>
2022-11-08 15:44:25 +0000

    learning about dictionaries

---
+++ dict01/pythondict01_error.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+## create a dictionary
+switch = {"hostname": "sw1", "ip": "10.0.1.1", "version": "1.2", "vendor": "cisco"}
+
+## display parts of the dictionary
+print( switch["hostname"] )
+print( switch["ip"] )
+
+## request a 'fake' key
+print( switch["lynx"] )  
+

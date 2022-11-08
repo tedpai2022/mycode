fc2e8a7526174d738c31c9c0eced2dc0f7043c37
tedpai2022 <gtedpai1415@gmail.com>
2022-11-08 16:18:03 +0000

    learning about dictionaries

--- dict01/pythondict01.py
+++ dict01/pythondict01.py
@@ -1,47 +1,28 @@
 #!/usr/bin/env python3
+"""Understanding dictionaries
+   {key: value, key:value, ...} """
 
-## create a dictionary
-switch = {"hostname": "sw1", "ip": "10.0.1.1", "version": "1.2", "vendor": "cisco"}
+def main():
+    """runtime function"""
 
-## display parts of the dictionary
-print( switch["hostname"] )
-print( switch["ip"] )
+    ## create a dictionary with 4 key:value pairs
+    switch = {"hostname": "sw1", "ip": "10.0.1.1", "version": "1.2", "vendor": "cisco"}
 
-## request a 'fake' key
-# print( switch["lynx"] )   # Be sure to comment out this line,
-                            # or your program will CONTINUE to fail!
-                            # if a KEY is requested that does not exist,
-                            # an ERROR will be thrown!
+    ## display the entire dictionary
+    print(switch)
 
-## request a 'fake' key with .get() method
-print( "First test - .get()" )
-print( switch.get("lynx") )
+    ## proove that switch is indeed a dictionary
+    print(type(switch))
 
-print( "Second test - .get()" )
-print( switch.get("lynx", "THE KEY IS IN ANOTHER CASTLE!") )
+    ## display parts of the dictionary
+    print( switch["hostname"] )    # displays "sw1"
+    print( switch["ip"] )          # displays "10.0.1.1"
 
-print( "Third test - .get()" )
-print( switch.get("version") )
-####add the foll###
-print( "Fourth test - .keys()" )
-print( switch.keys() )
+    ## request a 'fake' key
+    print( switch["lynx"] )  # this will cause the program to FAIL  
 
-print( "Fifth test - .values()" )
-print( switch.values() )
 
-#### add the foll.
-print( "Sixth test - .pop()" )
-switch.pop("version") # removes this key (and value) pair
-print( switch.keys() )   # notice the value of version is gone
-print( switch.values() ) # notice the value 1.2
-
-print( "Seventh test - ADD a new value" )
-switch["adminlogin"] = "karl08"
-print( switch.keys() )
-print( switch.values() )
-
-print( "Eighth test - ADD a new value" )
-switch["password"] = "qwerty"
-print( switch.keys() )
-print( switch.values() )
+# call our main function
+if __name__ == "__main__":
+    main()
 

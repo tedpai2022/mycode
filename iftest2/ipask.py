378e9e5c0854f680972f159cc31f59835eb3ef4c
tedpai2022 <gtedpai1415@gmail.com>
2022-11-08 20:01:37 +0000

    writing custom code

--- iftest2/ipask.py
+++ iftest2/ipask.py
@@ -8,3 +8,10 @@ ipchk = input("Apply an IP address: ") # this line now prompts the user for inpu
 if ipchk:
    print("Looks like the IP address was set: " + ipchk) # indented under if
 
+# add the foll.
+# a provided string will test true
+if ipchk:
+   print("Looks like the IP address was set: " + ipchk) # indented under if
+   # add the foll.
+else:    # if data is NOT provided
+   print("You did not provide input.") # indented under else
--- iftest2/ipask2.py
+++ iftest2/ipask2.py
@@ -7,5 +7,9 @@ ipchk = input("Apply an IP address: ") # this line now prompts the user for inpu
 # a provided string will test true
 if ipchk:
    print("Looks like the IP address was set: " + ipchk) # indented under if
+elif ipchk: # if any data is provided, this will test true
+   print("Looks like the IP address was set: " + ipchk) # indented under if
 else:    # if data is NOT provided
    print("You did not provide input.") # indented under else
+   # Add the foll.
+
